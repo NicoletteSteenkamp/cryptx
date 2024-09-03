45d2a5b60394f26bbf3748bc5435e46d339ba642
Junaid Alexander <junaidalexander76@gmail.com>
2024-09-03 19:41:30 +0100

    started to work on the sidebar
    Signed-off-by: Junaid Alexander <junaidalexander76@gmail.com>

---
+++ src/components/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Sidebar() {
+  return (
+    <div className='sidebar'>
+      <div className="logo">
+        <img src='' alt='' />
+      </div>
+      <div className="sidebar-menu">
+        <div className="sidebar-item">
+          
+        </div>
+      </div>
+    </div>
+  )
+}
