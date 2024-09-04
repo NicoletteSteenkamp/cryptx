b0d5a51708ab2855e46c7157758444a07176a9c4
Junaid Alexander <junaidalexander76@gmail.com>
2024-09-04 17:48:43 +0100

    working progress
    Signed-off-by: Junaid Alexander <junaidalexander76@gmail.com>

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,21 +1,44 @@
-import React from 'react'
+import React from "react";
+import "./Sidebar.css";
+import { MdDashboard } from "react-icons/md";
+import { BiWallet } from "react-icons/bi";
+import { MdClose, MdMenu } from "react-icons/md";
+import { FiPieChart, FiMail, FiSettings, FiLogOut } from "react-icons/fi";
 
 export default function Sidebar() {
-  return (
-    <div className='sidebar'>
-      <div className="logo">
-        <img src='' alt='' />
-      </div>
-      <div className="sidebar-menu">
-       <ul>
-        <li><h3>Overview</h3></li>
-        <li><h3>Chart</h3></li>
-        <li><h3>Transactions</h3></li>
-        <li><h3>Mail Box</h3></li>
-        <li><h3>Settings</h3></li>
-        <li><h3>Logout</h3></li>
-      </ul>
-      </div>
-    </div>
-  )
+	return (
+		<div className="sidebar">
+			<div className="logo">
+				<img src="" alt="" />
+			</div>
+			<div className="sidebar-menu">
+				<ul>
+					<li>
+						{" "}<MdDashboard />
+						<h3>Overview</h3>
+					</li>
+					<li>
+						<FiPieChart />
+						<h3>Chart</h3>
+					</li>
+					<li>
+						{" "}<BiWallet />
+						<h3>Transactions</h3>
+					</li>
+					<li>
+						<FiMail />
+						<h3>Mail Box</h3>
+					</li>
+					<li>
+						<FiSettings />
+						<h3>Settings</h3>
+					</li>
+					<li>
+						<FiLogOut />
+						<h3>Logout</h3>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
