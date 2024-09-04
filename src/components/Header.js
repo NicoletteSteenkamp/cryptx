ade512046d35d9962db295fefc9ae9ebe56c7efb
Junaid Alexander <junaidalexander76@gmail.com>
2024-09-04 19:29:12 +0100

    started to work on the  header Signed-off-by: Junaid Alexander <junaidalexander76@gmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 const header = () => {
   return (
-    <div>
-      
+    <div className='Header'> 
+      <div className="navbar">
+        navbar
+        <div className="search">
+          <input />
+        </div>
+      </div>
     </div>
   )
 }
