c1af3bc30b9a25d4081a7bcaf9b7aefa46ed5ae3
Junaid Alexander <junaidalexander76@gmail.com>
2024-09-05 18:21:33 +0100

    Your commit message describing the changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,25 @@
 import React from "react";
+import "./Header.css";
+import "font-awesome/css/font-awesome.min.css";
 
-const header = () => {
+const Header = () => {
 	return (
 		<div className="Header">
-			Header
+			
 			<div className="navbar">
-				navbar
+				
 				<div className="search">
-					<input />
+					<input type="search" placeholder="Search type of keywords" />
+					<i className="fa fa-search search-icon" aria-hidden="true" />
 				</div>
+				<div className="icons">
+					<i className="fa fa-bell-slash" aria-hidden="true" />
+					question mark
+				</div>
+				<div className="profile">icon user-name name</div>
 			</div>
 		</div>
 	);
 };
 
-export default header;
+export default Header;
