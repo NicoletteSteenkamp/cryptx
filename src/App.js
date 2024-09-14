f711dc4fbc837989d27198d2b444699de89c3d31
Junaid Alexander <junaidalexander76@gmail.com>
2024-09-14 19:55:25 +0100

    making progress Signed-off-by: Junaid Alexander <junaidalexander76@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,62 +1,67 @@
-import { useEffect } from "react";  
-import { Route, Routes, Navigate, useNavigate, useLocation } from "react-router-dom";  
-import Sidebar from "./components/Sidebar";  
-import Header from "./components/Header";  
-import "./App.css";  
-import Login from "./pages/SignIn";  
-import Signup from "./pages/Register";   
-import { SignedIn, useAuth } from "@clerk/clerk-react";   
-import styled from "styled-components";  
+import { useEffect } from "react";
+import {
+	Route,
+	Routes,
+	Navigate,
+	useNavigate,
+	useLocation
+} from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+import Crypto from "./components/crypto";
+import "./App.css";
+import Login from "./pages/SignIn";
+import Signup from "./pages/Register";
+import { SignedIn, useAuth } from "@clerk/clerk-react";
+import styled from "styled-components";
 
-const Wrap = styled.div`
-  display: flex;
-`;
+const Wrap = styled.div`display: flex;`;
 
-const Content = styled.div`
-  flex: 1;
-`;
+const Content = styled.div`flex: 1;`;
 
 export default function App() {
-  const { isSignedIn } = useAuth();
-  const navigate = useNavigate();
-  const location = useLocation();
+	const { isSignedIn } = useAuth();
+	const navigate = useNavigate();
+	const location = useLocation();
 
-  useEffect(() => {
-    if (isSignedIn) {
-      if (location.pathname === "/signup" || location.pathname === "/login") {
-        navigate("/app");
-      }
-    } else {
-      if (location.pathname !== "/login") {
-        navigate("/login");
-      }
-    }
-  }, [isSignedIn, navigate, location.pathname]);
+	useEffect(
+		() => {
+			if (isSignedIn) {
+				if (location.pathname === "/signup" || location.pathname === "/login") {
+					navigate("/app");
+				}
+			} else {
+				if (location.pathname !== "/login") {
+					navigate("/login");
+				}
+			}
+		},
+		[isSignedIn, navigate, location.pathname]
+	);
 
-  return (  
-    <Routes>  
-      <Route path="/login" element={<Login />} />  
-      <Route path="/signup" element={<Signup />} />  
-      <Route  
-        path="/app"  
-        element={  
-          <SignedIn>  
-            <Wrap>  
-              <Sidebar />  
-              <Content>  
-                <Header />  
-              </Content>  
-            </Wrap>  
-          </SignedIn>  
-        }  
-      />  
-      <Route  
-        path="/"  
-        element={  
-          isSignedIn ? <Navigate to="/app" /> : <Navigate to="/login" />  
-        }  
-      />  
-      <Route path="*" element={<Navigate to="/" />} />  
-    </Routes>  
-  );  
+	return (
+		<Routes>
+			<Route path="/login" element={<Login />} />
+			<Route path="/signup" element={<Signup />} />
+			<Route
+				path="/app"
+				element={
+					<SignedIn>
+						<Wrap>
+							<Sidebar />
+							<Content>
+								<Header />
+								<Crypto />
+							</Content>
+						</Wrap>
+					</SignedIn>
+				}
+			/>
+			<Route
+				path="/"
+				element={isSignedIn ? <Navigate to="/app" /> : <Navigate to="/login" />}
+			/>
+			<Route path="*" element={<Navigate to="/" />} />
+		</Routes>
+	);
 }
