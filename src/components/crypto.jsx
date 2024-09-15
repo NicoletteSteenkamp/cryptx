262a59d94fa2cd1a840406955b10608c625da379
Junaid Alexander <junaidalexander76@gmail.com>
2024-09-15 14:49:52 +0100

    still busty with the stylying Signed-off-by: Junaid Alexander <junaidalexander76@gmail.com>

---
+++ src/components/crypto.jsx
@@ -0,0 +1,221 @@
+import styles from "./crypto.module.css";
+
+function Crypto() {
+  return (
+    <div>
+      <div className={styles.div}>
+        <div className={styles.div2}>
+          <div className={styles.div3}>
+            <div className={styles.column}>
+              <div className={styles.div4}>
+                <div className={styles.div5}>
+                  <div className={styles.div6}>
+                    <div className={styles.column}>
+                      <div className={styles.cardContent}>
+                        <div className={styles.div7}>
+                          <div className={styles.div8}>
+                            <img
+                              loading="lazy"
+                              src="https://cdn.builder.io/api/v1/image/assets/TEMP/89d246e4220feb31d6c934676417a9e8a61a8cce812acb6a30545d785fd9d599?placeholderIfAbsent=true&apiKey=90d00836b5a049b48c0253add244096a"
+                              className={styles.img}
+                              alt="Bitcoin"
+                            />
+                            <div className={styles.div9}>
+                              <img
+                                loading="lazy"
+                                src="https://cdn.builder.io/api/v1/image/assets/TEMP/0e2a7fd3df1e496c95977ce345d83c590ebf7d1fa3bd2fda71cc6f05ae18aff9?placeholderIfAbsent=true&apiKey=90d00836b5a049b48c0253add244096a"
+                                className={styles.img2}
+                                alt="Change"
+                              />
+                              <div>+0.25%</div>
+                            </div>
+                          </div>
+                          <div className={styles.amount}>$40,291</div>
+                          <div className={styles.text}>Bitcoin - BTC</div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className={styles.column2}>
+                      <div className={styles.cardContent}>
+                        <div className={styles.div10}>
+                          <div className={styles.div11}>
+                            <img
+                              loading="lazy"
+                              src="https://cdn.builder.io/api/v1/image/assets/TEMP/e7ae911116d829f9a021ff27c3406c4eb6546d9333e22aa68c92444cbad120ac?placeholderIfAbsent=true&apiKey=90d00836b5a049b48c0253add244096a"
+                              className={styles.img}
+                              alt="Ethereum"
+                            />
+                            <div className={styles.div12}>
+                              <img
+                                loading="lazy"
+                                src="https://cdn.builder.io/api/v1/image/assets/TEMP/0e2a7fd3df1e496c95977ce345d83c590ebf7d1fa3bd2fda71cc6f05ae18aff9?placeholderIfAbsent=true&apiKey=90d00836b5a049b48c0253add244096a"
+                                className={styles.img3}
+                                alt="Change"
+                              />
+                              <div>+0.25%</div>
+                            </div>
+                          </div>
+                          <div className={styles.amount}>$18,291</div>
+                          <div className={styles.text}>Ethereum - ETH</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.div13}>
+                  <div className={styles.div14}>
+                    <div className={styles.column}>
+                      <div className={styles.cardContent2}>
+                        <div className={styles.div15}>
+                          <div className={styles.div16}>
+                            <img
+                              loading="lazy"
+                              src="https://cdn.builder.io/api/v1/image/assets/TEMP/2ed096e10da2d178839063d727a17fb26c4605cacfbe730fabec36bd0e69bf37?placeholderIfAbsent=true&apiKey=90d00836b5a049b48c0253add244096a"
+                              className={styles.img}
+                              alt="Litecoin"
+                            />
+                            <div className={styles.div17}>
+                              <img
+                                loading="lazy"
+                                src="https://cdn.builder.io/api/v1/image/assets/TEMP/cf8ccb6db837301c3c4716f1d747d5c463f5e61683da1e9ad9226278d8288443?placeholderIfAbsent=true&apiKey=90d00836b5a049b48c0253add244096a"
+                                className={styles.img4}
+                                alt="Change"
+                              />
+                              <div>+0.25%</div>
+                            </div>
+                          </div>
+                          <div className={styles.amount2}>$8,291</div>
+                          <div className={styles.text}>Litecoin - ITL</div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className={styles.column3}>
+                      <div className={styles.cardContent3}>
+                        <div className={styles.div18}>
+                          <div className={styles.div19}>
+                            <img
+                              loading="lazy"
+                              src="https://cdn.builder.io/api/v1/image/assets/TEMP/c685a08c5766b6bce435c60f010e822ef44240958ff0e78786de2e239641b706?placeholderIfAbsent=true&apiKey=90d00836b5a049b48c0253add244096a"
+                              className={styles.img}
+                              alt="Cardano"
+                            />
+                            <div className={styles.div20}>
+                              <img
+                                loading="lazy"
+                                src="https://cdn.builder.io/api/v1/image/assets/TEMP/048a04a288b22be392cfda57bd5dd3254a44e25414e001f6e78c4274de0f0633?placeholderIfAbsent=true&apiKey=90d00836b5a049b48c0253add244096a"
+                                className={styles.img5}
+                                alt="Change"
+                              />
+                              <div>-2.05%</div>
+                            </div>
+                          </div>
+                          <div className={styles.amount3}>$3,291</div>
+                          <div className={styles.text2}>Cardano - ADA</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.column4}>
+              <div className={styles.statistic}>
+                <div className={styles.div21}>
+                  <div className={styles.heading}>BTC Prices</div>
+                  <div className={styles.amount4}>$800</div>
+                  <div className={styles.div22}>
+                    <div className={styles.div23}>
+                      <div>$600</div>
+                      <div className={styles.amount5}>$400</div>
+                      <div className={styles.amount6}>$200</div>
+                      <div className={styles.amount7}>0</div>
+                    </div>
+                    <div className={styles.div24}>
+                      <div className={styles.div25}>
+                        <img
+                          loading="lazy"
+                          src="https://cdn.builder.io/api/v1/image/assets/TEMP/d0b6a9634ba2efc1d3304dfe21ee4b4813344b86b7b653f7451129b97760434c?placeholderIfAbsent=true&apiKey=90d00836b5a049b48c0253add244096a"
+                          className={styles.img6}
+                          alt="BTC Prices"
+                        />
+                        <div className={styles.div26}>
+                          <div className={styles.div27} />
+                          <div className={styles.div26}>$25,240</div>
+                        </div>
+                      </div>
+                      <div className={styles.div28} />
+                      <div className={styles.months}>
+                        <div className={styles.text3}>Jan</div>
+                        <div className={styles.text4}>Mar</div>
+                        <div className={styles.text5}>May</div>
+                        <div className={styles.text6}>Jul</div>
+                        <div className={styles.text7}>Sep</div>
+                        <div className={styles.text8}>Nov</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.column6}>
+              <div className={styles.liveMarket}>
+                <div className={styles.div31}>
+                  <div className={styles.heading2}>Live Market</div>
+                  <div className={styles.div32}>
+                    <div className={styles.div}>
+                      <img
+                        loading="lazy"
+                        src="https://cdn.builder.io/api/v1/image/assets/TEMP/c339dfe254dc110159f63fae9f3b7cb00e238f757d528b67f501a92d386d2d9e?placeholderIfAbsent=true&apiKey=90d00836b5a049b48c0253add244096a"
+                        className={styles.img7}
+                        alt="Live Market"
+                      />
+                      <img
+                        loading="lazy"
+                        src="https://cdn.builder.io/api/v1/image/assets/TEMP/45eae7019b62db6b5a6b8b13db00cb4dd8b38f9e049c0044d6a0d0177c0ac227?placeholderIfAbsent=true&apiKey=90d00836b5a049b48c0253add244096a"
+                        className={styles.img8}
+                        alt="Live Market"
+                      />
+                    </div>
+                    <div className={styles.div33}>
+                      <div>BTC Price</div>
+                      <div className={styles.amount8}>$800</div>
+                    </div>
+                    <div className={styles.div34}>
+                      <div>ETH Price</div>
+                      <div className={styles.amount9}>$600</div>
+                    </div>
+                    <div className={styles.div35}>
+                      <div>ADA Price</div>
+                      <div className={styles.amount10}>$200</div>
+                    </div>
+                    <div className={styles.div36}>
+                      <div>ITL Price</div>
+                      <div className={styles.amount11}>$100</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.column7}>
+              <div className={styles.chart}>
+                <div className={styles.div37}>
+                  <div className={styles.heading3}>Price Chart</div>
+                  <div className={styles.chartContainer}>
+                    <img
+                      loading="lazy"
+                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/0e2a7fd3df1e496c95977ce345d83c590ebf7d1fa3bd2fda71cc6f05ae18aff9?placeholderIfAbsent=true&apiKey=90d00836b5a049b48c0253add244096a"
+                      className={styles.chartImg}
+                      alt="Chart"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Crypto;
